a4357a2b96c677c35d78c77288db1f96740b6b39
DevM2021 <developeur.mad@gmail.com>
2021-04-10 17:26:12 +0300

    add config route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { LoginComponent } from './login/login/login.component';
+import { SignupComponent } from './signup/signup/signup.component';
+import { DetailComponent } from './vehicles/detail/detail/detail.component';
+import { VehicleComponent } from './vehicles/vehicle/vehicle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+
+  {
+    path: 'vehicles',
+    component: VehicleComponent,
+  },
+  {
+    path: 'vehicle-detail/:id',
+    component: DetailComponent,
+  },
+  {
+    path: 'auth/login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: SignupComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
