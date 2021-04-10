acae87ac661eddd27ec8ccfa55ab473468e80be4
DevM2021 <developeur.mad@gmail.com>
2021-04-10 17:23:16 +0300

    add required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { LoginComponent } from './login/login/login.component';
+import { SignupComponent } from './signup/signup/signup.component';
+import { CommentComponent } from './comment/comment/comment.component';
+import { VehicleComponent } from './vehicles/vehicle/vehicle.component';
+import { DetailComponent } from './vehicles/detail/detail/detail.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent, SignupComponent, CommentComponent, VehicleComponent, DetailComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
